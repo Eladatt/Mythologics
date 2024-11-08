1510d64844773c688889bbc46e0b46121fe23e63
Eladatt <eladatt@gmail.com>
2024-11-08 22:59:02 +0200

    Update base.js

--- base.js
+++ base.js
@@ -2,8 +2,9 @@ class ASCIIAnimator {
     constructor(asciiArt, info) {
         this.originalArt = asciiArt.trim().split('\n');
         this.matrix = this.asciiToMatrix(this.originalArt);
-        this.animatedArt = this.originalArt;
-        this.info = info;
+        this.animatedArt = this.originalArt; // Default to static ASCII art
+        this.info = info; // Anesthesia information
+        this.hue = 0; // Hue for color animation
     }
 
     asciiToMatrix(ascii) {
@@ -15,14 +16,13 @@ class ASCIIAnimator {
     }
 
     animate(t) {
-        // This method should be overridden by each creature's specific animation
-    }
-
-    render(container) {
-        container.innerHTML = `<pre>${this.animatedArt}</pre>`;
+        // Override in subclasses for specific animations
+        this.hue = (this.hue + t * 10) % 360; // Rotate hue over time
+        document.getElementById('ascii-container').style.color = `hsl(${this.hue}, 100%, 50%)`;
+        return this.matrixToAscii(this.matrix); // Return static art by default
     }
 
     getInfo() {
-        return this.info;
+        return this.info; // Return anesthesia information
     }
 }
